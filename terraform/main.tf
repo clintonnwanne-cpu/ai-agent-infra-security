94e6f1500e0a10542a02f0ba5ea842a7afd8eaa7
Clinton Nwanne <clintonnwanne@gmail.com>
2026-05-25 21:14:55 -0400

    Update variables and fixes from live deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,7 +69,7 @@ module "eks" {
 
   eks_managed_node_groups = {
     agent_nodes = {
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.small"]
       min_size       = 1
       max_size       = 2
       desired_size   = 1
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,7 +13,7 @@ variable "cluster_name" {
 variable "kubernetes_version" {
   description = "EKS Kubernetes version"
   type        = string
-  default     = "1.29"
+  default     = "1.31"
 }
 
 variable "environment" {
